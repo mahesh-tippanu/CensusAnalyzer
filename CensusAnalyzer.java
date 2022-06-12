ad14041fc435f683e6a129e8b406a6bacfb8db21
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-06-12 16:54:30 +0530

    added .java files main loop

---
+++ CensusAnalyzer.java
@@ -0,0 +1,31 @@
+package Census_Analyzer;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class CensusAnalyzer {
+           public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+            try {
+                Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+                CsvToBeanBuilder<IndianCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+                csvToBeanBuilder.withType(IndianCensusCSV.class);
+                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+                CsvToBean<IndianCensusCSV> csvToBean = csvToBeanBuilder.build();
+                Iterator<IndianCensusCSV> censusCSVIterator = csvToBean.iterator();
+                int namOfEateries = 0;
+                while (censusCSVIterator.hasNext()) {
+                    namOfEateries++;
+                    IndianCensusCSV censusData = censusCSVIterator.next();
+                }
+                return namOfEateries;
+            } catch (IOException e) {
+                throw new CensusAnalyserException(e.getMessage(),
+                        CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+            }
+        }
+    }
+
